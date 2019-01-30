81bb94b0423d36c5a17634bbd7ed885f83c7280a
Miia19 <45197265+Miia19@users.noreply.github.com>
2019-01-30 17:03:08 +0100

    exo1

--- Javascript/js/exo01.js
+++ Javascript/js/exo01.js
@@ -1,26 +1,50 @@
 var myUsers = [
-    {name:"Miia",birthday:"1992-04-19"},
-    {name:"kenza",birthday:"1993-06-20"},
-    {name:"leila",birthday:"1991-02-13"},
-    {name:"asmae",birthday:"1997-05-22"}
+    { Pseudonyme:"Miia", nom:"Nejjari", prenom: "Mounia", email: "miia@live.fr", naissance:"1992-04-19", address: "5 rue pasteur", code_postal: "93380", profession: "secrétaire" },
+    { Pseudonyme:"Kenoush",  nom:"Khelafi", prenom: "Kenza", email: "kenoush@live.fr", naissance:"1993-06-20", address: "5 rue pasteur", code_postal: "93380", profession: "secrétaire" },
+    { Pseudonyme:"Leiloush",  nom:"Taleb", prenom: "Leila", email: "leiloush@live.fr", naissance:"1991-02-13", address: "5 rue pasteur", code_postal: "93380", profession: "secrétaire" },
+    { Pseudonyme:"Assoue",  nom:"Nejjari", prenom: "Asmae", email: "assoue@live.fr", naissance:"1997-05-22", address: "5 rue pasteur", code_postal: "93380", profession: "secrétaire" }
    ];
    
-   function envoie(){
-      var userName = prompt("Quel est votre prénom ?");
-      var userBirthday = prompt("Quand êtes vous né ? ( ATTENTION Format AAAA-MM-JJ )");
+   // function envoie(){
+   //    var userName = prompt("Quel est votre prénom ?");
+   //    var userBirthday = prompt("Quand êtes vous né ? ( ATTENTION Format AAAA-MM-JJ )");
    
-      myUsers.push({name: userName , birthday: userBirthday});
-   }
+   //    myUsers.push({name: userName , birthday: userBirthday});
+   // }
    
    console.log(myUsers);
    
    function send(){
-   
-      var formValid = document.getElementById('bouton_envoi');
+      // var déclare une variable
+      var pseudonyme = document.getElementById('pseudonyme').value;
+      var nom = document.getElementById('nom').value;
       var prenom = document.getElementById('prenom').value;
       var naissance = document.getElementById('naissance').value;
-   
-      myUsers.push({name: prenom, birthday: naissance});
-   
-   
+      var email = document.getElementById('email').value;
+      var adresse = document.getElementById('adresse').value;
+      var code_postal= document.getElementById('codepostal').value;
+      var profession = document.getElementById('profession').value;
+
+      myUsers.push(
+         {
+            Pseudonyme: pseudonyme,
+            nom: nom,
+            prenom: prenom,
+            email: email,
+            naissance: naissance,
+            address: adresse,
+            code_postal: code_postal,
+            profession: profession
+         }
+      );   
    }
+var affichUtilisateurs = "nejjari";
+
+
+for (i=0; i<myUsers.length; i++){
+   affichUtilisateurs += myUsers[i].Pseudonyme +"<br>";
+   };
+
+document.getElementById('afficherutilisateurs').innerHTML = affichUtilisateurs;
+
+
